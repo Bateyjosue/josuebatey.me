d7f5950cb57d0d8fb1d2123b48c891aa1e5a1545
Bateyjosue <josuebatey19@gmail.com>
2024-01-05 19:21:28 +0200

    feat: render sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import Sidebar from "./components/navigation/Sidebar"
+
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-yellow-400">Hello Josh</h1>
-    </>
+    <section className="wrapper-lg flex max-w-full max-h-full h-full min-h-screen bg-blue-950 text-white">
+      <Sidebar className="w-3/12 flex flex-col justify-around gap-12" />
+      <main className="w-10/12 border rounded-lg">
+        okkfkfk
+      </main>
+    </section>
   )
 }
 
